d95399e886924ff69c6dbb4b2a67e06ce3cb9fe9
svyas261405 <130838044+svyas261405@users.noreply.github.com>
2023-04-24 04:41:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
    fruit_choice = streamlit.text_input("What fruit would you like information about?")
